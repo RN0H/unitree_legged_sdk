73e1a042ffa0b67d884d2f3a69bbd78ce8e6cd92
RohanPan <rohpan@uw.edu>
2024-03-02 14:47:38 -0800

    changes in example walk

--- examples/example_walk.cpp
+++ examples/example_walk.cpp
@@ -49,41 +49,43 @@ void Custom::RobotControl()
     // printf("%f %f %f\n", state.motorState[3].q, state.motorState[4].q, state.motorState[5].q);
 
     cmd.mode = 0;      // 0:idle, default stand      1:forced stand     2:walk continuously
-    cmd.gaitType = 0;
-    cmd.speedLevel = 0;
     cmd.footRaiseHeight = 0;
     cmd.bodyHeight = 0;
-    cmd.euler[0]  = 0;
-    cmd.euler[1] = 0;
-    cmd.euler[2] = 0;
-    cmd.velocity[0] = 0.0f;
-    cmd.velocity[1] = 0.0f;
-    cmd.yawSpeed = 0.0f;
+
+    /* Init rpy in place of eulers[3] (which is used in 3.3.1)*/
+    
+    cmd.roll = 0.f;
+    cmd.pitch = 0.f;
+    cmd.yaw = 0.f;
+
+    /*Init forward and side speed in place of velocity[2](which is used in 3.2)*/
+    cmd.forwardSpeed = 0.f;
+    cmd.sideSpeed    = 0.f;
 
 
     if(motiontime > 0 && motiontime < 1000){
         cmd.mode = 1;
-        cmd.euler[0] = -0.3;
+        cmd.roll = -0.3;
     }
     if(motiontime > 1000 && motiontime < 2000){
         cmd.mode = 1;
-        cmd.euler[0] = 0.3;
+        cmd.roll = 0.3;
     }
     if(motiontime > 2000 && motiontime < 3000){
         cmd.mode = 1;
-        cmd.euler[1] = -0.2;
+        cmd.pitch = -0.2;
     }
     if(motiontime > 3000 && motiontime < 4000){
         cmd.mode = 1;
-        cmd.euler[1] = 0.2;
+        cmd.pitch = 0.2;
     }
     if(motiontime > 4000 && motiontime < 5000){
         cmd.mode = 1;
-        cmd.euler[2] = -0.2;
+        cmd.yaw = -0.2;
     }
     if(motiontime > 5000 && motiontime < 6000){
         cmd.mode = 1;
-        cmd.euler[2] = 0.2;
+        cmd.yaw = 0.2;
     }
     if(motiontime > 6000 && motiontime < 7000){
         cmd.mode = 1;
@@ -108,20 +110,17 @@ void Custom::RobotControl()
     }
     if(motiontime > 14000 && motiontime < 18000){
         cmd.mode = 2;
-        cmd.gaitType = 2;
-        cmd.velocity[0] = 0.4f; // -1  ~ +1
-        cmd.yawSpeed = 2;
+        cmd.forwardSpeed = 0.4f; // -1  ~ +1
         cmd.footRaiseHeight = 0.1;
         // printf("walk\n");
     }
     if(motiontime > 18000 && motiontime < 20000){
         cmd.mode = 0;
-        cmd.velocity[0] = 0;
+        cmd.forwardSpeed = 0;
     }
     if(motiontime > 20000 && motiontime < 24000){
         cmd.mode = 2;
-        cmd.gaitType = 1;
-        cmd.velocity[0] = 0.2f; // -1  ~ +1
+        cmd.forwardSpeed = 0.2f; // -1  ~ +1
         cmd.bodyHeight = 0.1;
         // printf("walk\n");
     }
